5a272aadd17ecdea784f41724771ead9c0953256
Valeri Karpov <val@karpov.io>
2021-09-16 09:51:33 -0400

    remove global state from expense example re: code review comment

--- expense/src/workflows.ts
+++ expense/src/workflows.ts
@@ -7,35 +7,38 @@ const { createExpense, payment } = createActivityHandle<typeof activities>({
   scheduleToCloseTimeout: '5 minutes',
 });
 
-let status: ExpenseStatus = ExpenseStatus.CREATED;
-
-const signalTrigger = new Trigger<ExpenseStatus.APPROVED | ExpenseStatus.REJECTED>();
 const defaultTimeoutMS = 10000;
 
-export const expense: Expense = (expenseId: string, timeoutMS = defaultTimeoutMS) => ({
-  async execute(): Promise<{ status: ExpenseStatus }> {
-    await createExpense(expenseId);
+export const expense: Expense = function(expenseId: string, timeoutMS = defaultTimeoutMS) {
+  let status: ExpenseStatus = ExpenseStatus.CREATED;
 
-    if (status === ExpenseStatus.CREATED) {
-      status = await Promise.race([
-        signalTrigger,
-        sleep(timeoutMS).then((): ExpenseStatus => ExpenseStatus.TIMED_OUT)
-      ]);
-    }
+  const signalTrigger = new Trigger<ExpenseStatus.APPROVED | ExpenseStatus.REJECTED>();
 
-    if (status === ExpenseStatus.APPROVED) {
-      await payment(expenseId);
-      status = ExpenseStatus.COMPLETED;
-    }
-
-    return { status };
-  },
-  signals: {
-    approve() {
-      signalTrigger.resolve(ExpenseStatus.APPROVED);
+  return {
+    async execute(): Promise<{ status: ExpenseStatus }> {
+      await createExpense(expenseId);
+  
+      if (status === ExpenseStatus.CREATED) {
+        status = await Promise.race([
+          signalTrigger,
+          sleep(timeoutMS).then((): ExpenseStatus => ExpenseStatus.TIMED_OUT)
+        ]);
+      }
+  
+      if (status === ExpenseStatus.APPROVED) {
+        await payment(expenseId);
+        status = ExpenseStatus.COMPLETED;
+      }
+  
+      return { status };
     },
-    reject() {
-      signalTrigger.resolve(ExpenseStatus.REJECTED);
+    signals: {
+      approve() {
+        signalTrigger.resolve(ExpenseStatus.APPROVED);
+      },
+      reject() {
+        signalTrigger.resolve(ExpenseStatus.REJECTED);
+      }
     }
-  }
-});
+  };
+};
