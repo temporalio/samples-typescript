fd7f739aca023768c86bd284b22de5ce2ccabc6e
Loren 🤓 <lorensr@gmail.com>
2021-10-08 17:44:08 -0400

    Enable tests to run without building

--- activities-examples/test/workflow.test.ts
+++ activities-examples/test/workflow.test.ts
@@ -1,35 +1,40 @@
-import { Connection, WorkflowClient } from '@temporalio/client';
+import { WorkflowClient } from '@temporalio/client';
 import { Worker, DefaultLogger } from '@temporalio/worker';
 import { describe, before, after, afterEach, it } from 'mocha';
-import { example } from '../lib/workflows';
 import assert from 'assert';
 import axios from 'axios';
 import sinon from 'sinon';
 
-describe('example workflow', function() {
+import * as activities from '../src/activities';
+import { example } from '../src/workflows';
+
+describe('example workflow', function () {
   let runPromise = null;
   let worker = null;
   let workflow = null;
 
-  before(async function() {
-    this.timeout(10000);
+  this.slow(1000);
+
+  before(async function () {
+    this.timeout(10 * 1000);
     worker = await Worker.create({
-      workDir: `${__dirname}/../lib`,
-      taskQueue: 'testhttp',
+      workflowsPath: `${__dirname}/../src/workflows`,
+      nodeModulesPath: `${__dirname}/../node_modules`,
+      activities,
+      taskQueue: 'test-activities',
       logger: new DefaultLogger('ERROR'),
     });
 
     runPromise = worker.run();
   });
 
-  beforeEach(function() {
-    const connection = new Connection();
-    const client = new WorkflowClient(connection.service);
+  beforeEach(function () {
+    const client = new WorkflowClient();
 
-    workflow = client.createWorkflowHandle(example, { taskQueue: 'testhttp' });
+    workflow = client.createWorkflowHandle(example, { taskQueue: 'test-activities', workflowExecutionTimeout: 1000 });
   });
 
-  after(async function() {
+  after(async function () {
     worker.shutdown();
     await runPromise;
   });
@@ -38,12 +43,12 @@ describe('example workflow', function() {
     axios.get.restore && axios.get.restore();
   });
 
-  it('returns correct result', async function() {
+  it('returns correct result', async function () {
     const result = await workflow.execute();
     assert.equal(result, 'The answer is 42');
   });
 
-  it('retries one failure', async function() {
+  it('retries one failure', async function () {
     // Make the first request fail, but subsequent requests succeed
     let numCalls = 0;
     sinon.stub(axios, 'get').callsFake(() => {
@@ -57,10 +62,13 @@ describe('example workflow', function() {
     assert.equal(result, 'The answer is 88');
   });
 
-  it('bubbles up activity errors', async function() {
+  it('bubbles up activity errors', async function () {
     sinon.stub(axios, 'get').callsFake(() => Promise.reject(new Error('example error')));
 
-    const err = await workflow.execute().then(() => null, err => err);
+    const err = await workflow.execute().then(
+      () => null,
+      (err) => err
+    );
     assert.equal(err.name, 'WorkflowExecutionFailedError');
     assert.equal(err.cause.cause.message, 'example error');
   });
