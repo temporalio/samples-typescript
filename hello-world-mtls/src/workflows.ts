8c0c8c8db10583f4f6893275092f961c2da95e7d
Roey Berman <roey.berman@gmail.com>
2021-10-10 22:31:38 -0400

    Update hello-world* samples to use WF API r3

---
+++ hello-world-mtls/src/workflows.ts
@@ -0,0 +1,12 @@
+import { createActivityHandle } from '@temporalio/workflow';
+// Only import the activity types
+import type * as activities from './activities';
+
+const { greet } = createActivityHandle<typeof activities>({
+  startToCloseTimeout: '1 minute',
+});
+
+/** A workflow that simply calls an activity */
+export async function example(name: string): Promise<string> {
+  return await greet(name);
+}
