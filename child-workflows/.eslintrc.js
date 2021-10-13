11633ea88c404ad1d5d4371a1838c203bb30ee26
Loren 🤓 <lorensr@gmail.com>
2021-10-13 11:59:50 -0400

    Get eslint working from root

---
+++ child-workflows/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+};
