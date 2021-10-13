aa3a2f48fb2343550cfc8362b874a328a02968d9
Roey Berman <roey.berman@gmail.com>
2021-10-13 01:07:31 -0400

    Update signals and queries sample

--- signals-queries/src/execute-workflow.ts
+++ signals-queries/src/execute-workflow.ts
@@ -1,20 +1,16 @@
 import { Connection, WorkflowClient } from '@temporalio/client';
-import { unblockOrCancel } from './workflows';
+import { unblockOrCancel, unblockSignal, isBlockedQuery } from './workflows';
 
 async function run(): Promise<void> {
-  // Connect to localhost with default ConnectionOptions,
-  // pass options to the Connection constructor to configure TLS and other settings.
   const connection = new Connection();
-  // Workflows will be started in the "default" namespace unless specified otherwise
-  // via options passed the WorkflowClient constructor.
   const client = new WorkflowClient(connection.service);
-  // Create a typed handle for the example Workflow.
   const handle = client.createWorkflowHandle(unblockOrCancel, { taskQueue: 'tutorial' });
   await handle.start();
-  console.log(await handle.query.isBlocked()); // true
-  await handle.signal.unblock();
+  console.log('blocked?', await handle.query(isBlockedQuery)); // true
+  // To cancel instead of completing the Workflow, replace signal with: await handle.cancel()
+  await handle.signal(unblockSignal);
   await handle.result();
-  console.log(await handle.query.isBlocked()); // false
+  console.log('blocked?', await handle.query(isBlockedQuery)); // false
 }
 
 run().catch((err) => {
--- signals-queries/src/worker.ts
+++ signals-queries/src/worker.ts
@@ -1,18 +1,10 @@
 import { Worker } from '@temporalio/worker';
-import path from 'path';
 
 async function run() {
-  // Worker connects to localhost by default and uses console error for logging.
-  // Customize the Worker by passing more options to create().
-  // create() tries to connect to the server and will throw if a connection could not be established.
-  // You may create multiple Workers in a single process in order to poll on multiple task queues.
-  // In order to configure the server connection parameters and other global options,
-  // use the Core.install() method to configure the Rust Core SDK singleton.
-  const worker = await Worker.create({ 
-    workflowsPath: path.join(__dirname, 'workflows'),
-    nodeModulesPath: path.join(__dirname, '../node_modules'),
-    taskQueue: 'tutorial' });
-  // Start accepting tasks on the `tutorial` queue
+  const worker = await Worker.create({
+    workflowsPath: require.resolve('./workflows'),
+    taskQueue: 'tutorial',
+  });
   await worker.run();
 }
 
