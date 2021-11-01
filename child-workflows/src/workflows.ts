5dd1367797cf2455e388ee041db4b34d571a11c6
sw-yx <shawnthe1@gmail.com>
2021-11-01 04:38:44 -0700

    search attributes

--- child-workflows/src/workflows.ts
+++ child-workflows/src/workflows.ts
@@ -2,7 +2,17 @@
 import { executeChild } from '@temporalio/workflow';
 
 export async function parentWorkflow(names: string[]): Promise<string> {
-  const responseArray = await Promise.all(names.map((name) => executeChild(childWorkflow, { args: [name] })));
+  const responseArray = await Promise.all(
+    names.map((name) =>
+      executeChild(childWorkflow, {
+        args: [name],
+        // workflowId, // add business-meaningful workflow id here
+        // // regular workflow options apply here, with two additions (defaults shown):
+        // cancellationType: ChildWorkflowCancellationType.WAIT_CANCELLATION_COMPLETED,
+        // parentClosePolicy: ParentClosePolicy.PARENT_CLOSE_POLICY_TERMINATE
+      })
+    )
+  );
   return responseArray.join('\n');
 }
 // @@@SNIPEND
