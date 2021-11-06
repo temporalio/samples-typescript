dfe01b22e109c8d367ba39f6151ce83665d01ede
Loren 🤓 <lorensr@gmail.com>
2021-11-05 20:04:34 -0400

    Don't require Mailgun to run timer-examples

--- timer-examples/src/starter/fast.ts
+++ timer-examples/src/starter/fast.ts
@@ -1,15 +1,16 @@
-import { Connection, WorkflowClient } from '@temporalio/client';
+import { WorkflowClient } from '@temporalio/client';
 import { processOrderWorkflow } from '../workflows';
 
 async function run(): Promise<void> {
-  const connection = new Connection();
-  const client = new WorkflowClient(connection.service);
+  const client = new WorkflowClient();
 
-  await client.execute(processOrderWorkflow, {
+  // Does *not* send email to `process.env.ADMIN_EMAIL` that order processing is slow
+  const result = await client.execute(processOrderWorkflow, {
     taskQueue: 'tutorial20210928',
     args: [{ orderProcessingMS: 100, sendDelayedEmailTimeoutMS: 1000 }],
   });
-  console.log('Done'); // Does **not** send email to `process.env.ADMIN_EMAIL` that order processing is slow
+
+  console.log(result);
 }
 
 run().catch((err) => {
