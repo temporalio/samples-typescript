9a67158a14b72a37deea9c1be6fc861682fba640
sw-yx <shawnthe1@gmail.com>
2021-12-06 17:02:08 -0800

    add variable bindings

--- dsl-interpreter/src/workflows.ts
+++ dsl-interpreter/src/workflows.ts
@@ -27,28 +27,30 @@ const acts = proxyActivities<typeof activities>({
 
 /** A workflow that simply calls an activity */
 export async function DSLInterpreter(dsl: DSL): Promise<any> {
-  return execute(dsl.root);
+  const bindings = dsl.variables as Record<string, string>;
+  return execute(dsl.root, bindings);
 }
 
-async function execute(statement: Statement): Promise<string | undefined> {
-  const results = {} as Record<string, string | undefined>;
-  // console.log(statement)
+async function execute(
+  statement: Statement,
+  bindings: Record<string, string | undefined>
+): Promise<string | undefined> {
   if ('parallel' in statement) {
-    await Promise.all(statement.parallel.branches.map(execute));
+    await Promise.all(statement.parallel.branches.map((el) => execute(el, bindings)));
     // todo: if one activity fails we want to cancel all the others
   } else if ('sequence' in statement) {
     let result: string | undefined;
     for (const el of statement.sequence.elements) {
-      result = await execute(el);
+      result = await execute(el, bindings);
     }
     return result;
   } else {
     const activity = statement.activity;
     let args = activity.arguments || [];
-    args = args.map((arg) => results[arg] ?? arg);
+    args = args.map((arg) => bindings[arg] ?? arg);
     const activityResult = await acts[activity.name](...args);
     if (activity.result) {
-      results[activity.result] = activityResult;
+      bindings[activity.result] = activityResult;
     }
     return activityResult;
   }
