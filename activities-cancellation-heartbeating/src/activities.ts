a7157d6e8e70d2cdce15b28cded0566bbb2faebe
swyx <shawnthe1@gmail.com>
2022-02-11 12:39:02 +0800

    improve guidance for heartbeating

--- activities-cancellation-heartbeating/src/activities.ts
+++ activities-cancellation-heartbeating/src/activities.ts
@@ -4,8 +4,11 @@ import { CancelledFailure } from '@temporalio/common';
 
 export async function fakeProgress(sleepIntervalMs = 1000): Promise<void> {
   try {
-    for (let progress = 1; progress <= 100; ++progress) {
-      // sleep for given interval or throw if Activity is cancelled
+    // allow for resuming from heartbeat
+    const startingPoint = Context.current().info.heartbeatDetails || 1;
+    for (let progress = startingPoint; progress <= 100; ++progress) {
+      // simple utility to sleep in activity for given interval or throw if Activity is cancelled
+      // don't confuse with Workflow.sleep which is only used in Workflow functions!
       await Context.current().sleep(sleepIntervalMs);
       Context.current().heartbeat(progress);
     }
