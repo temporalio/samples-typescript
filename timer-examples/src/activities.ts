d6f3207559c6301612b5c1656981ecbdade434ba
Valeri Karpov <val@karpov.io>
2022-04-11 16:57:52 -0400

    remove deprecated mailgun-js in favor of axios
    Fix #123

--- timer-examples/src/activities.ts
+++ timer-examples/src/activities.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Context } from '@temporalio/activity';
-import mailgun from 'mailgun-js';
+import axios from 'axios';
 
 export interface EmailSettings {
   to: string | undefined;
   from: string;
 }
 
+const subject = 'Order processing taking longer than expected';
 const html = `Order processing is taking longer than expected, but don't worry—the job is still running!`;
 
-export const createActivities = (mg: mailgun.Mailgun | undefined, { to, from }: EmailSettings) => ({
+export const createActivities = ({ apiKey, domain }: { apiKey?: string, domain?: string }, { to, from }: EmailSettings) => ({
   async processOrder(sleepMS = 1000): Promise<void> {
     await Context.current().sleep(sleepMS);
   },
@@ -17,15 +18,16 @@ export const createActivities = (mg: mailgun.Mailgun | undefined, { to, from }: 
   async sendNotificationEmail(): Promise<void> {
     console.log('Sending email:', html);
 
-    if (mg && to) {
-      const data = {
-        to,
-        from,
-        subject: 'Order processing taking longer than expected',
-        html,
-      };
-
-      await mg.messages().send(data);
+    if (apiKey && domain && to) {
+      await axios({
+        url: 'https://api.mailgun.net/v3/' + domain + '/messages',
+        method: 'post',
+        params: { to, from, subject, html },
+        auth: {
+          username: 'api',
+          password: apiKey
+        }
+      });
     }
   },
 });
