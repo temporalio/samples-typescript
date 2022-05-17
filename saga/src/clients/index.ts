494ab6d2abe909fcaf127473902dbc151a38a1c0
Mike Nichols <mike.nichols@temporal.io>
2022-05-17 19:37:42 -0400

    more better async func

--- saga/src/clients/index.ts
+++ saga/src/clients/index.ts
@@ -2,54 +2,47 @@ import { Accounts, Banking, BoundedContextClients, Clients, PostOffice } from '.
 import { Commands } from '../types/commands';
 
 class ClientsServiceClient implements Clients.Client {
-  removeClient(params: Commands.RemoveClient): Promise<void> {
+  async removeClient(params: Commands.RemoveClient): Promise<void> {
     console.log('REMOVING CLIENT', params);
-    return Promise.resolve(undefined);
   }
 
-  addClient(params: Commands.AddClient): Promise<void> {
+  async addClient(params: Commands.AddClient): Promise<void> {
     if (params.shouldThrow) {
       throw new Error(params.shouldThrow);
     }
     console.log('ADDING CLIENT', params);
-    return Promise.resolve(undefined);
   }
 }
 class AccountsClient implements Accounts.Client {
-  createAccount(params: Commands.CreateAccount): Promise<void> {
+  async createAccount(params: Commands.CreateAccount): Promise<void> {
     if (params.shouldThrow) {
       throw new Error(params.shouldThrow);
     }
     console.log('CREATING ACCOUNT', params);
-    return Promise.resolve(undefined);
   }
 }
 class BankingClient implements Banking.Client {
-  addBankAccount(params: Commands.AddBankAccount): Promise<void> {
+  async addBankAccount(params: Commands.AddBankAccount): Promise<void> {
     if (params.shouldThrow) {
       throw new Error(params.shouldThrow);
     }
     console.log('ADDING BANK ACCOUNT', params);
-    return Promise.resolve(undefined);
   }
 
-  disconnectBankAccounts(params: Commands.DisconnectBankAccounts): Promise<void> {
+  async disconnectBankAccounts(params: Commands.DisconnectBankAccounts): Promise<void> {
     console.log('DISCONNECT BANK ACCOUNTS', params);
-    return Promise.resolve(undefined);
   }
 }
 class PostOfficeClient implements PostOffice.Client {
-  addAddress(params: Commands.AddPostalAddress): Promise<void> {
+  async addAddress(params: Commands.AddPostalAddress): Promise<void> {
     if (params.shouldThrow) {
       throw new Error(params.shouldThrow);
     }
     console.log('ADDING ADDRESS', params);
-    return Promise.resolve(undefined);
   }
 
-  clearPostalAddresses(params: Commands.ClearPostalAddresses): Promise<void> {
+  async clearPostalAddresses(params: Commands.ClearPostalAddresses): Promise<void> {
     console.log('CLEARING ADDRESSES', params);
-    return Promise.resolve(undefined);
   }
 }
 export const createClients = async (): Promise<BoundedContextClients> => {
