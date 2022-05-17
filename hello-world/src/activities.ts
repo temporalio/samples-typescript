170fa47418ad2f96aede551c4bed229618b8c6bd
Mike Nichols <mike.nichols@temporal.io>
2022-05-17 19:27:53 -0400

    revert misteaken update

--- hello-world/src/activities.ts
+++ hello-world/src/activities.ts
@@ -1,6 +1,5 @@
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
-  console.log('ACTIVITY LOG', name);
   return `Hello, ${name}!`;
 }
 // @@@SNIPEND
