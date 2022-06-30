ee5f54116dde7beb81f45e03bc066432d7a42214
Loren 🤓 <lorensr@gmail.com>
2022-06-29 17:02:42 -0700

    Fix workflow

--- search-attributes/src/workflows.ts
+++ search-attributes/src/workflows.ts
@@ -2,7 +2,7 @@ import { upsertSearchAttributes, workflowInfo } from '@temporalio/workflow';
 
 // @@@SNIPSTART typescript-search-attributes-workflow
 export async function example(): Promise<string> {
-  const customInt = workflowInfo().searchAttributes.CustomIntField[0];
+  const customInt = (workflowInfo().searchAttributes?.CustomIntField[0] as number) || 0;
   upsertSearchAttributes({
     // overwrite the existing value
     CustomIntField: [customInt + 1],
