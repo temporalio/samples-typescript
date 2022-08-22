8e930a50d1b81dc590d72f5eee47576240f2b211
Loren 🤓 <lorensr@gmail.com>
2022-08-22 12:14:30 -0400

    DRY

--- .scripts/upgrade-versions.mjs
+++ .scripts/upgrade-versions.mjs
@@ -11,28 +11,25 @@ if (!versionToSet) {
 for (const sample of STORED_SAMPLES) {
   const packagePath = path.join(sample, 'package.json');
   const content = JSON.parse(await fs.readFile(packagePath));
-  let changed = false;
-  for (const [dep, version] of Object.entries(content.dependencies)) {
-    if (dep.startsWith('@temporalio/')) {
-      if (version !== versionToSet) {
-        content.dependencies[dep] = versionToSet;
-        console.log(`Updated version for ${sample} dependency ${dep} from ${version} to ${versionToSet}`);
-        changed = true;
-      }
-    }
-  }
-  for (const [dep, version] of Object.entries(content.devDependencies)) {
-    if (dep.startsWith('@temporalio/')) {
-      if (version !== versionToSet) {
-        content.devDependencies[dep] = versionToSet;
-        console.log(`Updated version for ${sample} devDependency ${dep} from ${version} to ${versionToSet}`);
-        changed = true;
-      }
-    }
-  }
-  if (changed) {
+  const changedDeps = replaceDeps({ content, sample, field: 'dependencies' });
+  const changedDevDeps = replaceDeps({ content, field: 'devDependencies' });
+  if (changedDeps || changedDevDeps) {
     await fs.writeFile(packagePath, JSON.stringify(content));
   } else {
     console.log(`Version up to date for ${sample}`);
   }
+}
+
+function replaceDeps({ content, sample, field }) {
+  let changed = false;
+  for (const [dep, version] of Object.entries(content[field])) {
+    if (dep.startsWith('@temporalio/')) {
+      if (version !== versionToSet) {
+        content[field][dep] = versionToSet;
+        console.log(`Updated version for ${sample} ${field} ${dep} from ${version} to ${versionToSet}`);
+        changed = true;
+      }
+    }
+  }
+  return changed;
 }
