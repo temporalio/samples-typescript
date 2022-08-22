732ffa25bbab3ae3f98e65b79ddf6fae676c19fd
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2022-08-22 12:47:37 -0400

    Update for v1.1.0 (#190)
    * Update for v1.1.0
    
    * DRY

--- production/src/scripts/build-workflow-bundle.ts
+++ production/src/scripts/build-workflow-bundle.ts
@@ -3,16 +3,14 @@ import { writeFile } from 'fs/promises';
 import path from 'path';
 
 async function bundle() {
-  const { code, sourceMap } = await bundleWorkflowCode({
+  const { code } = await bundleWorkflowCode({
     workflowsPath: require.resolve('../workflows'),
   });
   const codePath = path.join(__dirname, '../../workflow-bundle.js');
-  const sourceMapPath = `${codePath}.map`;
 
   await writeFile(codePath, code);
-  await writeFile(sourceMapPath, sourceMap);
 
-  console.log(`Bundle written to ${codePath} (source maps: ${sourceMapPath})`);
+  console.log(`Bundle written to ${codePath}`);
 }
 
 bundle().catch((err) => {
