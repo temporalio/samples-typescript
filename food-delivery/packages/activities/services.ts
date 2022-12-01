407e71a4e0b594fa307698a248998e4728617898
Loren 🤓 <lorensr@gmail.com>
2022-11-30 23:26:54 -0500

    add idempotency token suffix

--- food-delivery/packages/activities/services.ts
+++ food-delivery/packages/activities/services.ts
@@ -13,7 +13,7 @@ export const notificationService = {
 export const paymentService = {
   charge(cents: number) {
     // In a real app, we would pass an idempotency token to the downstream service
-    const _idempotencyToken = Context.current().info.workflowExecution.workflowId
+    const _idempotencyToken = `${Context.current().info.workflowExecution.workflowId}-charge`
     if (cents >= 3500) {
       throw ApplicationFailure.create({ nonRetryable: true, message: 'Card declined: insufficient funds' })
     }
@@ -25,7 +25,7 @@ export const paymentService = {
 
   refund(cents: number) {
     // In a real app, we would pass an idempotency token to the downstream service
-    const _idempotencyToken = Context.current().info.workflowExecution.workflowId
+    const _idempotencyToken = `${Context.current().info.workflowExecution.workflowId}-refund`
     if (Math.random() < 0.7) {
       throw new Error('Failed to refund. Unable to reach payment service.')
     }
