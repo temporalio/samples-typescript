86d3b3212a58c408e4d55919f18c0051b690eae8
Valeri Karpov <val@karpov.io>
2023-01-07 13:27:19 -0500

    one worker per test and runUntil

--- mutex/src/test/workflows.test.ts
+++ mutex/src/test/workflows.test.ts
@@ -1,6 +1,6 @@
 import { TestWorkflowEnvironment } from '@temporalio/testing';
 import { WorkflowHandle } from '@temporalio/client';
-import { Runtime, DefaultLogger, Worker } from '@temporalio/worker';
+import { Runtime, DefaultLogger, Worker, WorkflowBundleWithSourceMap, bundleWorkflowCode } from '@temporalio/worker';
 import { describe, before, after, it } from 'mocha';
 import { lockWorkflow, oneAtATimeWorkflow } from '../workflows';
 import { currentWorkflowIdQuery } from '../shared';
@@ -15,23 +15,27 @@ describe('lock workflow', function () {
   let worker: Worker;
   let lockWorkflowHandle: WorkflowHandle<typeof lockWorkflow>;
   let env: TestWorkflowEnvironment;
+  let workflowBundle: WorkflowBundleWithSourceMap;
 
   before(async function () {
     this.timeout(10000);
     Runtime.install({ logger: new DefaultLogger('WARN') });
     env = await TestWorkflowEnvironment.create();
 
+    workflowBundle = await bundleWorkflowCode({
+      workflowsPath: require.resolve('../workflows'),
+      logger: new DefaultLogger('WARN'),
+    });
+  });
+
+  beforeEach(async function () {
     worker = await Worker.create({
       connection: env.nativeConnection,
-      workflowsPath: require.resolve('../workflows'),
+      workflowBundle,
       activities: activities.createActivities(env.client),
       taskQueue,
     });
 
-    runPromise = worker.run();
-  });
-
-  beforeEach(async function () {
     lockWorkflowHandle = await env.workflowClient.start(lockWorkflow, {
       taskQueue,
       workflowId: 'lock-' + nanoid(),
@@ -39,56 +43,57 @@ describe('lock workflow', function () {
   });
 
   after(async function () {
-    worker.shutdown();
-    await runPromise;
-
     await env.teardown();
   });
 
   it('handles locking and unlocking', async function () {
-    const testWorkflowId = 'test-' + nanoid();
-    const testWorkflowHandle = await env.workflowClient.start(oneAtATimeWorkflow, {
-      taskQueue,
-      workflowId: testWorkflowId,
-      args: [lockWorkflowHandle.workflowId],
+    await worker.runUntil(async function () {
+      const testWorkflowId = 'test-' + nanoid();
+      const testWorkflowHandle = await env.workflowClient.start(oneAtATimeWorkflow, {
+        taskQueue,
+        workflowId: testWorkflowId,
+        args: [lockWorkflowHandle.workflowId],
+      });
+
+      await env.sleep('100ms');
+
+      let currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
+      assert.equal(currentWorkflowId, testWorkflowId);
+
+      await testWorkflowHandle.result();
+
+      currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
+      assert.equal(currentWorkflowId, null);
     });
-
-    await env.sleep('100ms');
-
-    let currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
-    assert.equal(currentWorkflowId, testWorkflowId);
-
-    await testWorkflowHandle.result();
-
-    currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
-    assert.equal(currentWorkflowId, null);
   });
 
   it('automatically releases the lock after timeout', async function () {
-    const testWorkflowId1 = 'test-' + nanoid();
-    await env.workflowClient.start(oneAtATimeWorkflow, {
-      taskQueue,
-      workflowId: testWorkflowId1,
-      args: [lockWorkflowHandle.workflowId, 10000 /* 10s */],
+    await worker.runUntil(async function () {
+      const testWorkflowId1 = 'test-' + nanoid();
+      await env.workflowClient.start(oneAtATimeWorkflow, {
+        taskQueue,
+        workflowId: testWorkflowId1,
+        args: [lockWorkflowHandle.workflowId, 10000 /* 10s */],
+      });
+
+      await env.sleep('100ms');
+      let currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
+      assert.equal(currentWorkflowId, testWorkflowId1);
+
+      const testWorkflowId2 = 'test-' + nanoid();
+      await env.workflowClient.start(oneAtATimeWorkflow, {
+        taskQueue,
+        workflowId: testWorkflowId2,
+        args: [lockWorkflowHandle.workflowId, 10000 /* 10s */],
+      });
+
+      await env.sleep('100ms');
+      currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
+      assert.equal(currentWorkflowId, testWorkflowId1);
+
+      await env.sleep('1300ms');
+      currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
+      assert.equal(currentWorkflowId, testWorkflowId2);
     });
-
-    await env.sleep('100ms');
-    let currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
-    assert.equal(currentWorkflowId, testWorkflowId1);
-
-    const testWorkflowId2 = 'test-' + nanoid();
-    await env.workflowClient.start(oneAtATimeWorkflow, {
-      taskQueue,
-      workflowId: testWorkflowId2,
-      args: [lockWorkflowHandle.workflowId, 10000 /* 10s */],
-    });
-
-    await env.sleep('100ms');
-    currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
-    assert.equal(currentWorkflowId, testWorkflowId1);
-
-    await env.sleep('1300ms');
-    currentWorkflowId = await lockWorkflowHandle.query(currentWorkflowIdQuery);
-    assert.equal(currentWorkflowId, testWorkflowId2);
   });
 });
