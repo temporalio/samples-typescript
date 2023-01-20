b92a8a86f5f4e106172f2a834b8b7824c80c6731
Valeri Karpov <val@karpov.io>
2023-01-20 13:38:26 -0500

    add snipsync comments for #228

--- logger-shared/src/activities/index.ts
+++ logger-shared/src/activities/index.ts
@@ -1,6 +1,8 @@
+// @@@SNIPSTART typescript-shared-logger-activity
 import logger from '../sharedLogger';
 
 export async function greet(name: string): Promise<string> {
   logger.info('Log from Activity', { name });
   return `Hello, ${name}!`;
 }
+// @@@SNIPEND
--- logger-shared/src/sharedLogger.ts
+++ logger-shared/src/sharedLogger.ts
@@ -1,5 +1,7 @@
+// @@@SNIPSTART typescript-shared-logger
 import { proxySinks, inWorkflowContext } from '@temporalio/workflow';
 import logger from './logger';
 
 const sharedLogger = inWorkflowContext() ? proxySinks().defaultWorkerLogger : logger;
 export default sharedLogger;
+// @@@SNIPEND
