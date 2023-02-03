a5a3d5bee010de4f412657076e5d77a8841255a3
James Watkins-Harvey <mjameswh@gmail.com>
2023-02-03 12:08:45 -0500

    Fix incorrect file URL to Path conversion on Windows

--- fetch-esm/src/worker.ts
+++ fetch-esm/src/worker.ts
@@ -1,14 +1,14 @@
 // @@@SNIPSTART typescript-esm-worker
 import { Worker } from '@temporalio/worker';
-import { URL } from 'url';
+import { URL, fileURLToPath } from 'url';
 import path from 'path';
 import * as activities from './activities.js';
 
 // Support running both complied code and ts-node/esm loader
-const workflowsPath = new URL(`./workflows${path.extname(import.meta.url)}`, import.meta.url).pathname;
+const workflowsPathUrl = new URL(`./workflows${path.extname(import.meta.url)}`, import.meta.url);
 
 const worker = await Worker.create({
-  workflowsPath,
+  workflowsPath: fileURLToPath(workflowsPathUrl),
   activities,
   taskQueue: 'fetch-esm',
 });
