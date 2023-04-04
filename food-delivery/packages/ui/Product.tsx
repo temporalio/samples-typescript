bf1ac988e380f85241b1b5f8379b9187313e04dd
James Watkins-Harvey <mjameswh@gmail.com>
2023-04-04 15:01:58 -0700

    Fix obscure prettier errors

--- food-delivery/packages/ui/Product.tsx
+++ food-delivery/packages/ui/Product.tsx
@@ -36,7 +36,7 @@ export const Product = ({ product, disabled, loading, onOrder }: ProductProps) =
         <button
           onClick={() => onOrder(product)}
           disabled={disabled}
-          className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 py-2 px-8 text-sm font-medium text-gray-900 hover:bg-gray-200"
+          className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
         >
           {loading && <Loading />} Order<span className="sr-only">, {product.name}</span>
         </button>
