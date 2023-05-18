e36fab4192e61820ea7825b8c2a10625842d19a5
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2023-05-18 19:53:43 -0400

    Add vscode-debugger sample (#266)
    * Add vscode-debugger sample
    
    * Update ci.yml
    
    * Address comments
    
    * Update packages

---
+++ vscode-debugger/src/worker.ts
@@ -0,0 +1,17 @@
+import { Worker } from '@temporalio/worker';
+import * as activities from './activities';
+
+async function run() {
+  const worker = await Worker.create({
+    workflowsPath: require.resolve('./workflows'),
+    activities,
+    taskQueue: 'replay-demo',
+  });
+
+  await worker.run();
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
