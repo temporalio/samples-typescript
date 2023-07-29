77bf6a76a3653810beeae78cb7c136718909205a
Loren 🤓 <lorensr@gmail.com>
2023-07-29 18:01:43 -0400

    rm heartbeat

--- hello-world/src/activities.ts
+++ hello-world/src/activities.ts
@@ -2,7 +2,6 @@ import { Context } from '@temporalio/activity';
 
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
-  Context.current().heartbeat();
   return Context.current().info.heartbeatTimeoutMs?.toString() || 'undefined';
 }
 // @@@SNIPEND
