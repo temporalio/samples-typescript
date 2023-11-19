0600ca1c4002aa3f6beed44df071e2a43de8aa0a
Giulio Home <3272563+giuliohome@users.noreply.github.com>
2023-11-19 16:44:54 +0000

    cancellation aware sleep example

--- worker-specific-task-queues/src/activities.ts
+++ worker-specific-task-queues/src/activities.ts
@@ -13,6 +13,13 @@ export function createNormalActivities(uniqueWorkerTaskQueue: string) {
 function delay(ms: number) {
   return new Promise( resolve => setTimeout(resolve, ms) );
 }
+function tsleep(ms: number, cancelled: Promise<never>){
+  let handle: NodeJS.Timeout;
+  const timer = new Promise<void>((resolve) => {
+    handle = setTimeout(resolve, ms);
+  });
+  return Promise.race([cancelled.finally(() => clearTimeout(handle)), timer]);
+}
 export function createActivitiesForSameWorker() {
   return {
     async downloadFileToWorkerFileSystem(url: string, path: string): Promise<void> {
@@ -20,19 +27,22 @@ export function createActivitiesForSameWorker() {
       log.info('Downloading and saving', { url, path });
       // Here's where the real download code goes
       const body = Buffer.from('downloaded body');
-      await delay(3000);
+      log.info("cancellation aware sleep");
+      await tsleep(3000, Context.current().cancelled);
       await fs.writeFile(path, body);
     },
     async workOnFileInWorkerFileSystem(path: string): Promise<void> {
       const { log } = Context.current();
       const content = await fs.readFile(path);
       const checksum = createHash('md5').update(content).digest('hex');
-      await delay(3000);
+      log.info("cancellation aware sleep");
+      await tsleep(3000, Context.current().cancelled);
       log.info('Did some work', { path, checksum });
     },
     async cleanupFileFromWorkerFileSystem(path: string): Promise<void> {
       const { log } = Context.current();
-      await delay(3000);
+      log.info("cancellation aware sleep");
+      await tsleep(3000, Context.current().cancelled);
       log.info('Cleaning up temp file', { path });
       await fs.rm(path);
     },
