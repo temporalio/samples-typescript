7a58377e76b9e2920c29f353604f3b0e93006d5c
Loren 🤓 <lorensr@gmail.com>
2024-01-10 22:51:46 -0500

    format

--- food-delivery/apps/driver/pages/index.tsx
+++ food-delivery/apps/driver/pages/index.tsx
@@ -5,7 +5,7 @@ export default function DriverApp() {
   return (
     <div className="bg-white">
       <Header />
-     
+
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="mb-24 text-center text-4xl font-bold text-gray-900" id="app-name">
           Driver Portal
