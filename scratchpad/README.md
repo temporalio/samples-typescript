c7a071ed12c455a5c9114f66c5365b0340981236
Dan Davison <dan.davison@temporal.io>
2024-02-25 12:14:01 -0500

    Single-file "scratchpad" example (#343)

---
+++ scratchpad/README.md
@@ -0,0 +1,25 @@
+# Scratchpad
+
+This sample demonstrates a minimalistic single-file style that might be useful for quickly sketching out an idea, or for sharing a minimal reproduction of a bug.
+Production code should not be arranged like this! See the other samples in this repo for recommended project structure.
+
+### Running this sample
+
+In one terminal:
+
+```
+temporal server start-dev
+```
+
+In another terminal:
+
+```
+npm install temporalio
+ts-node scratchpad.ts
+```
+
+The following will be printed out:
+
+```
+Hello, Temporal!
+```
---
+++ scratchpad/scratchpad.ts
@@ -0,0 +1,52 @@
+import { proxyActivities, inWorkflowContext } from '@temporalio/workflow';
+import { Connection, Client, WorkflowIdReusePolicy } from '@temporalio/client';
+import { Worker, DefaultLogger, Runtime } from '@temporalio/worker';
+
+const workflowId = 'scratchpad';
+const taskQueue = 'scratchpad';
+
+const activities = {
+  async activity(name: string): Promise<string> {
+    return `Hello, ${name}!`;
+  },
+};
+
+const { activity } = proxyActivities<typeof activities>({
+  startToCloseTimeout: '1 minute',
+});
+
+export async function workflow(name: string): Promise<string> {
+  return await activity(name);
+}
+
+async function starter(client: Client): Promise<void> {
+  const result = await client.workflow.execute(workflow, {
+    taskQueue,
+    workflowId,
+    args: ['Temporal'],
+    workflowIdReusePolicy: WorkflowIdReusePolicy.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING,
+  });
+  console.log(result);
+}
+
+async function main(): Promise<void> {
+  const worker = await Worker.create({
+    workflowsPath: __filename,
+    activities,
+    taskQueue,
+    bundlerOptions: {
+      ignoreModules: ['@temporalio/client', '@temporalio/worker'],
+    },
+  });
+  const connection = await Connection.connect();
+  const client = new Client({ connection });
+  await worker.runUntil(starter(client));
+}
+
+if (!inWorkflowContext()) {
+  Runtime.install({ logger: new DefaultLogger('WARN') });
+  main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
