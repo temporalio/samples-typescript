5a7051bdfb639ebd23466c209ccff3bb04c70577
Dan Davison <dan.davison@temporal.io>
2024-12-10 17:45:21 -0500

    UwS Early return sample

---
+++ early-return/src/start-test-workflow.ts
@@ -0,0 +1,34 @@
+import { Connection, Client, StartWorkflowOperation } from '@temporalio/client';
+import { transactionWorkflow } from './workflows';
+import { getTransactionConfirmation } from './shared';
+
+async function runTransactionWorkflow(transactionID: string, client: Client) {
+  const workflowId = 'transaction-' + transactionID;
+
+  const startWorkflowOperation = new StartWorkflowOperation(transactionWorkflow, {
+    workflowId,
+    args: [transactionID],
+    taskQueue: 'early-return',
+  });
+
+  const earlyConfirmation = await client.workflow.executeUpdateWithStart(getTransactionConfirmation, {
+    startWorkflowOperation,
+  });
+  const wfHandle = await startWorkflowOperation.workflowHandle();
+  const finalReport = await wfHandle.result();
+
+  console.log(`early transaction confirmation: ${JSON.stringify(earlyConfirmation)}`);
+  console.log(`final transaction report: ${JSON.stringify(finalReport)}`);
+}
+
+async function main() {
+  const connection = await Connection.connect();
+  const client = new Client({ connection });
+  const transactionID = process.argv[2] || 'my-transaction-id';
+  await runTransactionWorkflow(transactionID, client);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
