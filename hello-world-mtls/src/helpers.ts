82b6d448fe4f2d8ef288f348183316669d7cf7e7
James Watkins-Harvey <mjameswh@users.noreply.github.com>
2025-01-15 09:33:57 -0500

    Improve TLS Hello World sample (#407)

---
+++ hello-world-mtls/src/helpers.ts
@@ -0,0 +1,49 @@
+import fs from 'fs/promises';
+
+export interface Env {
+  address: string;
+  namespace: string;
+
+  // TLS server validation — generally not needed if connecting to Temporal Cloud
+  serverNameOverride?: string;
+  serverRootCACertificate: Buffer | undefined;
+
+  // mTLS authentication
+  clientCert: Buffer | undefined;
+  clientKey: Buffer | undefined;
+
+  // API Key authentication
+  apiKey?: string;
+
+  taskQueue: string;
+}
+
+export async function getEnv(): Promise<Env> {
+  return {
+    address: requiredEnv('TEMPORAL_ADDRESS'),
+    namespace: requiredEnv('TEMPORAL_NAMESPACE'),
+
+    serverNameOverride: process.env.TEMPORAL_SERVER_NAME_OVERRIDE,
+    serverRootCACertificate: await maybeReadFileAsBuffer(process.env.TEMPORAL_SERVER_ROOT_CA_CERT_PATH),
+
+    clientCert: await maybeReadFileAsBuffer(process.env.TEMPORAL_CLIENT_CERT_PATH),
+    clientKey: await maybeReadFileAsBuffer(process.env.TEMPORAL_CLIENT_KEY_PATH),
+
+    apiKey: process.env.TEMPORAL_CLIENT_KEY_PATH,
+
+    taskQueue: process.env.TEMPORAL_TASK_QUEUE || 'hello-world-tls',
+  };
+}
+
+function requiredEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new ReferenceError(`${name} environment variable is not defined`);
+  }
+  return value;
+}
+
+async function maybeReadFileAsBuffer(path?: string): Promise<Buffer | undefined> {
+  if (path === undefined) return undefined;
+  return await fs.readFile(path);
+}
--- hello-world-mtls/src/worker.ts
+++ hello-world-mtls/src/worker.ts
@@ -1,39 +1,30 @@
-import fs from 'fs/promises';
-
 // @@@SNIPSTART typescript-mtls-worker
 import { Worker, NativeConnection } from '@temporalio/worker';
 import * as activities from './activities';
+import { getEnv } from './helpers';
 
 /**
- * Run a Worker with an mTLS connection, configuration is provided via environment variables.
- * Note that serverNameOverride and serverRootCACertificate are optional.
+ * Establish a Worker connection to a Temporal cluster using TLS encryption,
+ * with optional mTLS or API Key authentication.
  */
-async function run({
-  address,
-  namespace,
-  clientCertPath,
-  clientKeyPath,
-  serverNameOverride,
-  serverRootCACertificatePath,
-  taskQueue,
-}: Env) {
-  let serverRootCACertificate: Buffer | undefined = undefined;
-  if (serverRootCACertificatePath) {
-    serverRootCACertificate = await fs.readFile(serverRootCACertificatePath);
-  }
+async function run() {
+  const { address, namespace, serverNameOverride, serverRootCACertificate, clientCert, clientKey, apiKey, taskQueue } =
+    await getEnv();
 
   const connection = await NativeConnection.connect({
     address,
     tls: {
       serverNameOverride,
       serverRootCACertificate,
-      // See docs for other TLS options
-      clientCertPair: {
-        crt: await fs.readFile(clientCertPath),
-        key: await fs.readFile(clientKeyPath),
-      },
+      clientCertPair: clientKey &&
+        clientCert && {
+          crt: clientCert,
+          key: clientKey,
+        },
     },
+    apiKey,
   });
+
   try {
     const worker = await Worker.create({
       connection,
@@ -51,40 +42,8 @@ async function run({
   }
 }
 
-run(getEnv()).catch((err) => {
+run().catch((err) => {
   console.error(err);
   process.exit(1);
 });
 // @@@SNIPEND
-
-// Helpers for configuring the mTLS client and worker samples
-
-function requiredEnv(name: string): string {
-  const value = process.env[name];
-  if (!value) {
-    throw new ReferenceError(`${name} environment variable is not defined`);
-  }
-  return value;
-}
-
-export interface Env {
-  address: string;
-  namespace: string;
-  clientCertPath: string;
-  clientKeyPath: string;
-  serverNameOverride?: string;
-  serverRootCACertificatePath?: string;
-  taskQueue: string;
-}
-
-export function getEnv(): Env {
-  return {
-    address: requiredEnv('TEMPORAL_ADDRESS'),
-    namespace: requiredEnv('TEMPORAL_NAMESPACE'),
-    clientCertPath: requiredEnv('TEMPORAL_CLIENT_CERT_PATH'),
-    clientKeyPath: requiredEnv('TEMPORAL_CLIENT_KEY_PATH'),
-    serverNameOverride: process.env.TEMPORAL_SERVER_NAME_OVERRIDE,
-    serverRootCACertificatePath: process.env.TEMPORAL_SERVER_ROOT_CA_CERT_PATH,
-    taskQueue: process.env.TEMPORAL_TASK_QUEUE || 'hello-world-mtls',
-  };
-}
