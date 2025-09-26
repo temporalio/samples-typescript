74f244f55ce0ef9b61c6e8a60ff441e47d480149
Misha Bruml <misha.bruml@kaluza.com>
2025-09-26 16:07:20 +0100

    path

--- child-workflows/src/workflows-history.test.ts
+++ child-workflows/src/workflows-history.test.ts
@@ -1,11 +1,12 @@
 import { Worker } from '@temporalio/worker';
 import workflowHistory from './workflow-history.json';
+import path from 'path';
 
 describe('Workflow history compatibility', () => {
   test('Workflow history is compatible', async () => {
     await Worker.runReplayHistory(
       {
-        workflowsPath: require.resolve('./workflows.ts'),
+        workflowsPath: require.resolve(path.join(__dirname, 'workflows.ts')),
       },
       workflowHistory
     );
