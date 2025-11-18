413bb9e390f1533a55b743da5b1f6c972515647b
James Watkins-Harvey <mjameswh@users.noreply.github.com>
2025-11-18 13:51:38 -0500

    [fetch-esm] Address CI flakes and Node warning messages (#442)

--- fetch-esm/src/client-local.ts
+++ fetch-esm/src/client-local.ts
@@ -1,15 +1,15 @@
 // @@@SNIPSTART typescript-esm-client
 import { Client, Connection } from '@temporalio/client';
 import { loadClientConnectConfig } from '@temporalio/envconfig';
-import { example } from './workflows.js';
+import { exampleLocal } from './workflows.ts';
 
 const config = loadClientConnectConfig();
 const connection = await Connection.connect(config.connectionOptions);
 const client = new Client({ connection });
-const result = await client.workflow.execute(example, {
+const result = await client.workflow.execute(exampleLocal, {
   taskQueue: 'fetch-esm',
-  workflowId: 'my-business-id',
-  args: ['Temporal'],
+  workflowId: `my-business-id-${Date.now()}`,
+  args: ['Wonderful-Temporal'],
 });
-console.log(result); // Hello, Temporal!
+console.log(result); // Hello World And Hello Wonderful Temporal!
 // @@@SNIPEND
--- fetch-esm/src/workflows.ts
+++ fetch-esm/src/workflows.ts
@@ -1,13 +1,29 @@
 // @@@SNIPSTART typescript-esm-workflow
 import { proxyActivities } from '@temporalio/workflow';
 // Only import the activity types
-import type * as activities from './activities.js';
+import type * as activities from './activities.ts';
 
-const { greetHTTP } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '1 minute',
+const { greetLocal } = proxyActivities<typeof activities>({
+  scheduleToCloseTimeout: '5 seconds',
 });
 
-export async function example(name: string): Promise<string> {
-  return await greetHTTP(name);
+// A variant of the preceding workflow that does not perform any HTTP requests
+export async function exampleLocal(name: string): Promise<string> {
+  return await greetLocal(name);
 }
 // @@@SNIPEND
+
+// The sample `fetch` activity make requests to httpbin.org, which is sometime slow or flaky.
+// In the case of this workflow, we prefer to fail the workflow than retry the HTTP request forever.
+const { greetHTTP } = proxyActivities<typeof activities>({
+  startToCloseTimeout: '3 seconds',
+  retry: {
+    initialInterval: '500 ms',
+    maximumAttempts: 3,
+    backoffCoefficient: 1.5,
+  },
+});
+
+export async function exampleFetch(name: string): Promise<string> {
+  return await greetHTTP(name);
+}
