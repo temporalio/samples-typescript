6b586419317c2b3db52b5b04a7cc06a160087aa0
Maciej Dudkowski <maciej.dudkowski@temporal.io>
2026-05-05 00:58:55 -0700

    Removed comments from worker.ts

--- standalone-activity/src/worker.ts
+++ standalone-activity/src/worker.ts
@@ -3,32 +3,17 @@ import * as activities from './activities';
 import { loadClientConnectConfig } from '@temporalio/envconfig';
 
 async function run() {
-  // Step 1: Establish a connection with Temporal server.
-  //
-  // Worker code uses `@temporalio/worker.NativeConnection`.
-  // (But in your application code it's `@temporalio/client.Connection`.)
   const config = loadClientConnectConfig();
   const connection = await NativeConnection.connect(config.connectionOptions);
   try {
-    // Step 2: Register Activities with the Worker.
     const worker = await Worker.create({
       connection,
       namespace: 'default',
       taskQueue: 'hello-standalone-activities',
       activities,
     });
-
-    // Step 3: Start accepting tasks on the `hello-standalone-activities` queue
-    //
-    // The worker runs until it encounters an unexpected error or the process receives a shutdown signal registered on
-    // the SDK Runtime object.
-    //
-    // By default, worker logs are written via the Runtime logger to STDERR at INFO level.
-    //
-    // See https://typescript.temporal.io/api/classes/worker.Runtime#install to customize these defaults.
     await worker.run();
   } finally {
-    // Close the connection once the worker has stopped
     await connection.close();
   }
 }
