8907f2950c1d12936306667fca933c600a61cf7b
James Watkins-Harvey <mjameswh@users.noreply.github.com>
2026-09-16 17:52:34 -0400

    Remove retired VS Code debugger sample (#524)

--- vscode-debugger/src/activities.ts
+++
@@ -1,9 +0,0 @@
-import { log } from '@temporalio/activity';
-
-export async function notifyHumanForVerification(task: string): Promise<void> {
-  log.info(`🤖 Dear human, please verify that this is correct: ${task}`);
-}
-
-export async function collectFeedback(): Promise<void> {
-  log.info('🤖 Dear human, how was your experience with this verification workflow?');
-}
